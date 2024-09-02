85d2ea275fc2f41baf14d14f56ae18b3857adbbc
Mai El-Zein <mai_ez@outlook.dk>
2024-09-02 15:39:57 +0200

    igang med at lave dynamik view

---
+++ singleview.js
@@ -0,0 +1,19 @@
+const urlParas = new URLSearchParams(window.location.search);
+const id = urlParas.get("id");
+const url = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
+function getProduct() {
+  fetch(url)
+    .then((res) => res.json())
+    .then(view);
+}
+
+function view(produkt) {
+  document.querySelector("h3").textContent = produkt.productdisplayname;
+  document.querySelector(
+    "img"
+  ).src = `hhtps://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  document.querySelector("img").alt = produkt.productdisplayname;
+}
+
+getProduct();
