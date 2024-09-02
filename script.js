ea61eb7b0b6057f022918730cc29f60cd098275b
Mai El-Zein <mai_ez@outlook.dk>
2024-09-02 15:23:44 +0200

    Productlist med billeder virker

---
+++ script.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", init);
+console.log("Hello World!");
+
+const productsURI = `https://kea-alt-del.dk/t7/api/products?limit=5`;
+
+let productList;
+let productTemplate;
+
+function init() {
+  console.log("init");
+  productList = document.querySelector(".gallery");
+  productTemplate = document.querySelector("#template_product").content;
+
+  fetch(productsURI)
+    .then((res) => res.json())
+    .then(handleData);
+}
+
+function handleData(json) {
+  console.log("handleData");
+  json.forEach(showProduct);
+}
+
+function showProduct(product) {
+  console.log("product", product);
+  const clone = productTemplate.cloneNode(true);
+  clone.querySelector("h3").textContent = product.productdisplayname;
+  clone.querySelector("p").textContent = product.season;
+
+  const imgURI = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  clone.querySelector("img").src = imgURI;
+  clone.querySelector("img").alt = `Image og ${product.productdisplayname}`;
+  productList.appendChild(clone);
+}
