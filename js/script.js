afc060d6e7aa4776be1502de1b6b5e9f42a2efbe
Mai El-Zein <mai_ez@outlook.dk>
2024-09-03 13:57:28 +0200

    har lavet sould out og discount vigning i produktlist

--- js/script.js
+++ js/script.js
@@ -26,7 +26,23 @@ function showProduct(product) {
   const clone = productTemplate.cloneNode(true);
   clone.querySelector("h3").textContent = product.productdisplayname;
   clone.querySelector("p").textContent = product.season;
+  clone.querySelector(".before").textContent = product.price + ",-";
 
+  if (product.soldout) {
+    clone.querySelector(".buy_1").classList.add("soldout");
+    clone.querySelector(".buy_1").textContent = "Sold Out";
+  } else if (product.discount) {
+    clone.querySelector(".after").textContent =
+      "After:" +
+      " " +
+      Math.round(product.price - (product.price * product.discount) / 100) +
+      ",-";
+    clone.querySelector(".before").classList.add("offerprice");
+    clone.querySelector(".before").textContent =
+      "Before:" + " " + product.price;
+    clone.querySelector(".buy_1").classList.add("offer");
+    clone.querySelector(".buy_1").textContent = "OFFER!" + " " + "Buy now";
+  }
   const imgURI = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
   clone.querySelector("img").src = imgURI;
   clone.querySelector("img").alt = `Image og ${product.productdisplayname}`;
